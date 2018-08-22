4d242f094d0f3907128afc38faa2e240c4567786
strdr4605 <str.dr4605@gmail.com>
2018-08-22 20:42:43 +0300

    add routes for students

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from './config';
 import bodyParser from 'body-parser';
 import testRoutes from './routes/test';
+import studentRoutes from './routes/student';
 
 let app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use('/api/v1/test', testRoutes);
+app.use('/api/v1/student', studentRoutes);
 
 app.listen(config.port, () => {
   console.log('Application is running on port ' + config.port);
